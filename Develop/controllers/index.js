50897d0dde0ee5b0668c549526a5619ab1ce9aff
Zach Rojas <zachrojas94@gmail.com>
2021-11-13 13:30:52 -0600

    added code to bookRoutes

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,6 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-=======
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
@@ -9,4 +7,3 @@ router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
->>>>>>> afbfa2b849a73168c497b57c2bc31827ef132f07
