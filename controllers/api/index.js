12b698d8fc31071020ef9c9dcf89a3e58142bbc8
Zach Rojas <zachrojas94@gmail.com>
2021-11-19 13:43:46 -0600

    updatesss

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const bookRoutes = require('./bookRoutes');
+const shoppingCartRoutes = require('./shoppingCartRoutes');
 
 router.use('/users', userRoutes);
 router.use('/books', bookRoutes);
 router.use('/shopping', shoppingCartRoutes);
-
 
 module.exports = router;
 
