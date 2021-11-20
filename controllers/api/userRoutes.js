0f205ede66c554cb29cd17dc2cf1e57741852508
esfsc-90 <67880570+mjs-esfsc@users.noreply.github.com>
2021-11-19 18:40:29 -0600

    more updates to routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,7 +5,7 @@ const { User } = require("../../models");
 router.post("/signup", async (req, res) => {
   try {
     const userData = await User.create({
-      username: req.body.username,
+      name: req.body.username,
       email: req.body.email,
       password: req.body.password,
     });
